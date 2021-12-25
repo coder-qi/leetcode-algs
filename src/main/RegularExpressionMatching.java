d12f9ef16152084236d1359d584aec9313ec6480
coder-qi <zhangfengxian-test>
2021-12-25 17:16:08 +0800

    正则表达式匹配：https://leetcode-cn.com/problems/regular-expression-matching/

--- src/main/RegularExpressionMatching.java
+++ src/main/RegularExpressionMatching.java
@@ -21,8 +21,44 @@ public class RegularExpressionMatching {
         }
     }
 
+    /**
+     * 动态规划：
+     * p[j] != '*': if s[i] == p[j] || p[j] == '.', dp[i][j] = dp[i - 1][j - 1]
+     * p[j] == '*': if s[i] != p[j] && p[j] != '.', dp[i][j] = dp[i][j - 2]
+     * else dp[i][j] = dp[i - 1][j] // 多次匹配
+     * dp[i][j] = dp[i][j - 1] // 一次
+     * dp[i][j] = dp[i][j - 2] // 不匹配
+     *
+     * <p>时间复杂度：O(mn)
+     * <p>空间复杂度：O(mn)
+     */
     public static boolean isMatch(String s, String p) {
-        return false;
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < n; i++) {
+            if (p.charAt(i) == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                } else {
+                    if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 
     public static void main(String[] args) {
