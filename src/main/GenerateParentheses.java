5de364210d57836e06e5160f07ff00f65db59664
coder-qi <zhangfengxian-test>
2021-12-27 23:00:56 +0800

    括号生成：https://leetcode-cn.com/problems/generate-parentheses/

--- src/main/GenerateParentheses.java
+++ src/main/GenerateParentheses.java
@@ -6,13 +6,16 @@ import java.util.List;
  */
 public class GenerateParentheses {
 
-    public static List<String> generateParenthesis(int n) {
+    /**
+     * 使用回溯来解决
+     */
+    public static List<String> generateParenthesis_backtrace(int n) {
         List<String> result = new ArrayList<>();
-        generateParenthesis(result, 0, 0, n, new StringBuilder());
+        backtrace(result, 0, 0, n, new StringBuilder());
         return result;
     }
 
-    public static void generateParenthesis(List<String> result, int l, int r, int n, StringBuilder sb) {
+    public static void backtrace(List<String> result, int l, int r, int n, StringBuilder sb) {
        if (l > n || r > n || r > l) {
            return;
        }
@@ -21,12 +24,41 @@ public class GenerateParentheses {
            return;
        }
        sb.append('(');
-       generateParenthesis(result, l + 1, r, n, sb);
+       backtrace(result, l + 1, r, n, sb);
        sb.setLength(sb.length() - 1);
 
        sb.append(')');
-       generateParenthesis(result, l, r + 1, n, sb);
+       backtrace(result, l, r + 1, n, sb);
        sb.setLength(sb.length() - 1);
+    }
+
+    static ArrayList<String>[] cache = new ArrayList[10];
+
+    /**
+     * 按括号序列的长度递增，每一个括号序列都能用(a)b来表示
+     */
+    public static List<String> generateParenthesis(int n) {
+        return generate(n);
+    }
+
+    private static List<String> generate(int n) {
+        if (cache[n] != null) {
+            return cache[n];
+        }
+        ArrayList<String> result = new ArrayList<>();
+        if (n == 0) {
+            result.add("");
+        } else {
+            for (int c = 0; c < n; c++) {
+                for (String left : generate(c)) {
+                    for (String right : generate(n - c - 1)) {
+                        result.add("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+        cache[n] = result;
+        return result;
     }
 
     public static void main(String[] args) {
