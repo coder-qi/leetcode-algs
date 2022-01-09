072155d005eecb3be8bb25cf2ade06e4eaad9d53
coder-qi <zhangfengxian-test>
2022-01-09 23:13:22 +0800

    接雨水：https://leetcode-cn.com/problems/trapping-rain-water/

--- src/main/TrappingRainWater.java
+++ src/main/TrappingRainWater.java
@@ -3,7 +3,7 @@
  */
 public class TrappingRainWater {
 
-    public static int trap(int[] height) {
+    public static int trap_my(int[] height) {
         int maxIndex = 0, n = height.length;
         for (int i = 0, max = 0; i < n; i++) {
             if (height[i] > max) {
@@ -33,6 +33,33 @@ public class TrappingRainWater {
             index--;
         }
 
+        return ans;
+    }
+
+    /**
+     * 动态规划：求出位置i左右两边所能达到的最大高度，该位置的节水量即为：min(leftMax[i], rightMax[i]) - height[i]
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     */
+    public static int trap(int[] height) {
+        int n = height.length;
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
         return ans;
     }
 
