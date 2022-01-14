db59d054b0d7fefa1676c1512713761d8dfce1e7
coder-qi <zhangfengxian-test>
2022-01-14 08:11:22 +0800

    字母异位词分组：https://leetcode-cn.com/problems/group-anagrams/

---
+++ src/main/GroupAnagrams.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 字母异位词分组：https://leetcode-cn.com/problems/group-anagrams/
+ */
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String s = new String(chars);
+            map.computeIfAbsent(s, (k) -> new ArrayList<>()).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(groupAnagrams(new String[] {""}));
+        System.out.println(groupAnagrams(new String[] {"a"}));
+    }
+
+}
