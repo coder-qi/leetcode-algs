f0fea32c36da81a2143f3c0b36a9ebd75fe9f6ee
coder-qi <zhangfengxian-test>
2022-01-15 12:02:07 +0800

    N 皇后：https://leetcode-cn.com/problems/n-queens/

--- src/main/NQueens.java
+++ src/main/NQueens.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,30 +10,27 @@ public class NQueens {
     public static List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
         int[] queens = new int[n];
-        backtrace(n, queens, result, 0);
+        Arrays.fill(queens, -1);
+        backtrace(n, queens, result, 0, 0, 0, 0);
         return result;
     }
 
-    private static void backtrace(int n, int[] queens , List<List<String>> result, int row) {
+    private static void backtrace(int n, int[] queens , List<List<String>> result,
+        int row, int columns, int diagonals1, int diagonals2) {
         if (row == n) {
             result.add(toQueenList(queens));
             return;
         }
-        for (int i = 1; i <= n; i++) {
-            boolean b = true;
-            for (int j = 0; j < row; j++) {
-                if (queens[j] == i || row - j == Math.abs(queens[j] - i)) {
-                    b = false;
-                    break;
-                }
-            }
-            if (!b) {
-                continue;
-            }
+        int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+        while (availablePositions != 0) {
+            int position = availablePositions & (-availablePositions);
+            availablePositions = availablePositions & (availablePositions - 1);
+            int column = Integer.bitCount(position - 1);
 
-            queens[row] = i;
-            backtrace(n, queens, result, row + 1);
-            queens[row] = 0;
+            queens[row] = column;
+            backtrace(n, queens, result, row + 1, columns | position,
+                (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+            queens[row] = -1;
         }
     }
 
@@ -43,7 +41,7 @@ public class NQueens {
         for (int i = 0; i < n; i++) {
             sb.setLength(0);
             for (int j = 0; j < n; j++) {
-                if (queens[i] - 1 == j) {
+                if (queens[i] == j) {
                     sb.append('Q');
                 } else {
                     sb.append('.');
