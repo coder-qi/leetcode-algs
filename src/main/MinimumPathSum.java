395446e27ad603fbe9d2de0399d6f51e9217e580
张琪 <zhangqi@smartbi.com.cn>
2022-01-18 14:21:23 +0800

    最小路径和：https://leetcode-cn.com/problems/minimum-path-sum/

--- src/main/MinimumPathSum.java
+++ src/main/MinimumPathSum.java
@@ -5,20 +5,17 @@ public class MinimumPathSum {
 
     public static int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = grid[i][0] + dp[i - 1][0];
-        }
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = grid[0][i] + dp[0][i - 1];
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i][j - 1], dp[i - 1][j]);
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    dp[j] = (j - 1 >= 0 ? dp[j - 1] : 0) + grid[i][j];
+                } else {
+                    dp[j] = grid[i][j] + (j - 1 >= 0 ? Math.min(dp[j], dp[j - 1]) : dp[j]);
+                }
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
