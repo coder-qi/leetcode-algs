f115eca0214a993510bbd27ac4667e59e562d158
coder-qi <zhangfengxian-test>
2022-01-18 08:47:46 +0800

    最小路径和：https://leetcode-cn.com/problems/minimum-path-sum/

--- src/main/MinimumPathSum.java
+++ src/main/MinimumPathSum.java
@@ -15,7 +15,7 @@ public class MinimumPathSum {
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i][j - 1], dp[i - 1][j]);
+                dp[i][j] += grid[i][j] + Math.min(dp[i][j - 1], dp[i - 1][j]);
             }
         }
         return dp[m - 1][n - 1];
