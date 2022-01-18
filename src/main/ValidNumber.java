233e1e31c0af1b5ed4c6080156e7d56af03b3de0
coder-qi <zhangfengxian-test>
2022-01-18 22:15:45 +0800

    有效数字：https://leetcode-cn.com/problems/valid-number/

--- src/main/ValidNumber.java
+++ src/main/ValidNumber.java
@@ -1,48 +1,84 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 有效数字：https://leetcode-cn.com/problems/valid-number/
  */
 public class ValidNumber {
+    static Map<State, Map<CharType, State>> transfer = new HashMap<>();
+    static {
+        transfer.put(State.STATE_INITIAL, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+            put(CharType.CHAR_SIGN, State.STATE_INT_SIGN);
+        }});
+        transfer.put(State.STATE_INT_SIGN, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+        }});
+        transfer.put(State.STATE_INTEGER, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }});
+        transfer.put(State.STATE_POINT, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }});
+        transfer.put(State.STATE_POINT_WITHOUT_INT, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+        }});
+        transfer.put(State.STATE_FRACTION, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }});
+        transfer.put(State.STATE_EXP, new HashMap<>() {{
+            put(CharType.CHAR_SIGN, State.STATE_EXP_SIGN);
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }});
+        transfer.put(State.STATE_EXP_SIGN, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }});
+        transfer.put(State.STATE_EXP_NUMBER, new HashMap<>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }});
+    }
 
     public static boolean isNumber(String s) {
-        if (s == null || s.isEmpty()) {
-            return false;
-        }
-        int index = 0;
-        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
-            index++;
-        }
-        boolean allowSign = false;
-        boolean exp = false;
-        boolean digit = false;
-        boolean dot = false;
-        for (int i = index; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c >= '0' && c <= '9') {
-                digit = true;
-                if (exp) {
-                    allowSign = false;
-                }
-            } else if (c == '+' || c == '-') {
-                if (!allowSign || i == s.length() - 1) {
-                    return false;
-                }
-                allowSign = false;
-            } else if (c == '.') {
-                if (dot || exp || (i == s.length() - 1 && !digit)) {
-                    return false;
-                }
-                dot = true;
-            } else if (c == 'e' || c == 'E') {
-                if (exp || !digit || i == s.length() - 1) {
-                    return false;
-                }
-                allowSign = true;
-                exp = true;
-            } else {
+        State state = State.STATE_INITIAL;
+        for (int i = 0; i < s.length(); i++) {
+            CharType type = toCharType(s.charAt(i));
+            state = transfer.get(state).get(type);
+            if (state == null) {
                 return false;
             }
         }
-        return true;
+        return state == State.STATE_INTEGER || state == State.STATE_POINT
+            || state == State.STATE_FRACTION || state == State.STATE_EXP_NUMBER
+            || state == State.STATE_END;
+    }
+
+    private static CharType toCharType(char c) {
+        if (c >= '0' && c <= '9') {
+            return CharType.CHAR_NUMBER;
+        } else if (c == 'e' || c == 'E') {
+            return CharType.CHAR_EXP;
+        } else if (c == '.') {
+            return CharType.CHAR_POINT;
+        } else if (c == '+' || c == '-') {
+            return CharType.CHAR_SIGN;
+        }
+        return CharType.CHAR_ILLEGAL;
+    }
+
+    enum State {
+        STATE_INITIAL, STATE_INT_SIGN, STATE_INTEGER, STATE_POINT,
+        STATE_POINT_WITHOUT_INT, STATE_FRACTION, STATE_EXP,
+        STATE_EXP_SIGN, STATE_EXP_NUMBER, STATE_END
+    }
+
+    enum CharType {
+        CHAR_NUMBER, CHAR_EXP, CHAR_POINT, CHAR_SIGN, CHAR_ILLEGAL
     }
 
     public static void main(String[] args) {
