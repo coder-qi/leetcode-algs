bebed6e84e7212bd88c56d5e485d2f3a9a15d3a4
coder-qi <zhangfengxian-test>
2022-01-22 22:32:09 +0800

    爬楼梯：https://leetcode-cn.com/problems/climbing-stairs/

---
+++ src/main/ClimbingStairs.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 爬楼梯：https://leetcode-cn.com/problems/climbing-stairs/
+ */
+public class ClimbingStairs {
+
+    private static Map<Integer, Integer> map = new HashMap() {
+        {
+            put(1, 1);
+            put(2, 2);
+        }
+    };
+
+    public static int climbStairs(int n) {
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        int v = climbStairs(n - 1) + climbStairs(n - 2);
+        map.put(n, v);
+        return v;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2)); // 2
+        System.out.println(climbStairs(45)); // 2
+    }
+
+}
