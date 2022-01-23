f2c01c37855069e7f211397012028f3c3295ad84
coder-qi <zhangfengxian-test>
2022-01-23 20:38:51 +0800

    矩阵置零：https://leetcode-cn.com/problems/set-matrix-zeroes/

--- src/main/SetMatrixZeroes.java
+++ src/main/SetMatrixZeroes.java
@@ -7,42 +7,25 @@ public class SetMatrixZeroes {
 
     public static void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        boolean firstRowZero = false, firstColumnZero = false;
+        boolean firstColumnZero = false;
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 firstColumnZero = true;
-                break;
             }
-        }
-        for (int i = 0; i < n; i++) {
-            if (matrix[0][i] == 0) {
-                firstRowZero = true;
-                break;
-            }
-        }
-        for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
-        for (int i = 1; i < m; i++) {
+        for (int i = m - 1; i >= 0; i--) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
-
                 }
             }
-        }
-        if (firstColumnZero) {
-            for (int i = 0; i < m; i++) {
+            if (firstColumnZero) {
                 matrix[i][0] = 0;
-            }
-        }
-        if (firstRowZero) {
-            for (int i = 0; i < n; i++) {
-                matrix[0][i] = 0;
             }
         }
     }
