e1022e30df24d3eb75ca65a7f4b8a10f9bd7b17c
张琪 <zhangqi@smartbi.com.cn>
2022-02-11 11:47:43 +0800

    最小覆盖子串：https://leetcode-cn.com/problems/minimum-window-substring/

--- src/main/MinimumWindowSubstring.java
+++ src/main/MinimumWindowSubstring.java
@@ -1,69 +1,63 @@
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+import java.util.function.BiFunction;
 
 /**
  * 最小覆盖子串：https://leetcode-cn.com/problems/minimum-window-substring/
  */
 public class MinimumWindowSubstring {
 
+    private static BiFunction<Character, Integer, Integer> PLUS_ONE = (k, v) -> v == null ? 1 : v + 1;
+    private static BiFunction<Character, Integer, Integer> MINUS_ONE = (k, v) -> v <= 1 ? null : v - 1;
+
     public static String minWindow(String s, String t) {
         Map<Character, Integer> ori = new HashMap<>();
         for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            ori.put(c, ori.getOrDefault(c, 0) + 1);
+            ori.compute(t.charAt(i), PLUS_ONE);
         }
 
         Map<Character, Integer> cnt = new HashMap<>(ori);
         // 记录多余的字符数量
         Map<Character, Integer> extraCnt = new HashMap<>();
+        // 记录有效字符的位置
+        Queue<Integer> invalidPositions = new LinkedList<>();
         int left = 0, right = -1;
         int len = Integer.MAX_VALUE, resultLeft = -1, resultRight = -1;
         while (right < s.length()) {
             right++;
             if (right < s.length() && ori.containsKey(s.charAt(right))) {
-                if (!cnt.containsKey(s.charAt(right))) {
-                    extraCnt.put(s.charAt(right), extraCnt.getOrDefault(s.charAt(right), 0) + 1);
-                } else {
-                    if (cnt.get(s.charAt(right)) <= 1) {
-                        cnt.remove(s.charAt(right));
-                    } else {
-                        cnt.put(s.charAt(right), cnt.getOrDefault(s.charAt(right), 0) - 1);
-                    }
-                }
+                invalidPositions.add(right);
+                removeCnt(cnt, extraCnt, s.charAt(right));
             }
-            while (cnt.isEmpty() && left <= right) {
+            while (cnt.isEmpty() && !invalidPositions.isEmpty() && left <= right) {
+                left = invalidPositions.remove();
                 if (right - left + 1 < len) {
                     len = right - left + 1;
                     resultLeft = left;
                     resultRight = left + len;
                 }
-                if (ori.containsKey(s.charAt(left))) {
-                    if (extraCnt.containsKey(s.charAt(left))) {
-                        if (extraCnt.get(s.charAt(left)) <= 1) {
-                            extraCnt.remove(s.charAt(left));
-                        } else {
-                            extraCnt.put(s.charAt(left), extraCnt.getOrDefault(s.charAt(left), 0) - 1);
-                        }
-                    } else {
-                        cnt.put(s.charAt(left), cnt.getOrDefault(s.charAt(left), 0) + 1);
-                    }
-                }
-                left++;
+                addCnt(cnt, extraCnt, s.charAt(left));
             }
         }
-
         return resultLeft == -1 ? "" : s.substring(resultLeft, resultRight);
     }
 
-    private static boolean check(Map<Character, Integer> ori, Map<Character, Integer> cnt) {
-        for (Map.Entry<Character, Integer> entry : ori.entrySet()) {
-            Character key = entry.getKey();
-            Integer val = entry.getValue();
-            if (cnt.getOrDefault(key, 0) < val) {
-                return false;
-            }
+    private static void removeCnt(Map<Character, Integer> cnt, Map<Character, Integer> extraCnt, char c) {
+        if (!cnt.containsKey(c)) {
+            extraCnt.compute(c, PLUS_ONE);
+        } else {
+            cnt.compute(c, MINUS_ONE);
         }
-        return true;
+    }
+
+    private static void addCnt(Map<Character, Integer> cnt, Map<Character, Integer> extraCnt, char c) {
+        if (extraCnt.containsKey(c)) {
+            extraCnt.compute(c, MINUS_ONE);
+        } else {
+            cnt.compute(c, PLUS_ONE);
+        }
     }
 
     public static void main(String[] args) {
