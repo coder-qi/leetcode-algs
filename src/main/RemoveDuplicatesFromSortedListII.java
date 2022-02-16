44b70a479e8df6c2260328ffd69072e57a2249d6
coder-qi <zhangfengxian-test>
2022-02-16 22:41:20 +0800

    删除排序链表中的重复元素 II：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/

--- src/main/RemoveDuplicatesFromSortedListII.java
+++ src/main/RemoveDuplicatesFromSortedListII.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 /**
  * 删除排序链表中的重复元素 II：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/
  */
@@ -11,13 +9,29 @@ public class RemoveDuplicatesFromSortedListII {
         }
         ListNode dummy = new ListNode(0, head);
 
+        ListNode cur = head;
+        ListNode prev = dummy;
+        while (cur != null && cur.next != null) {
+            if (cur.val != cur.next.val) {
+                prev = cur;
+                cur = cur.next;
+            } else {
+                int v = cur.val;
+                while (cur != null && cur.val == v) {
+                    cur = cur.next;
+                }
+                prev.next = cur;
+            }
+        }
+
         head = dummy.next;
         dummy.next = null;
         return head;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(deleteDuplicates(ListNode.of(1,2,3,3,4,4,5,5)));
+        System.out.println(deleteDuplicates(ListNode.of(1,1,1,2,3)));
     }
 
 }
