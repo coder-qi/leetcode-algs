84da1f60e73a1e69359cf1f412d65a9aab05090b
coder-qi <zhangfengxian-test>
2022-02-20 23:20:32 +0800

    柱状图中最大的矩形：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
    单调栈+优化
    时间复杂度：O(n)
    空间复杂度：O(n)

--- src/main/LargestRectangleInHistogram.java
+++ src/main/LargestRectangleInHistogram.java
@@ -1,4 +1,5 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,20 +15,14 @@ public class LargestRectangleInHistogram {
         int n = heights.length;
         int[] left = new int[n];
         int[] right = new int[n];
+        Arrays.fill(right, n);
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                right[stack.peek()] = i;
                 stack.pop();
             }
             left[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
-        }
-        stack.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
-                stack.pop();
-            }
-            right[i] = stack.isEmpty() ? n : stack.peek();
             stack.push(i);
         }
         int ans = 0;
