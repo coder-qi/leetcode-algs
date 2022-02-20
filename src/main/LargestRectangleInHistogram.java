07a82e1cbb48732350e8e5110dbbf89a0b05a3dd
coder-qi <zhangfengxian-test>
2022-02-20 22:17:46 +0800

    柱状图中最大的矩形：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/ 使用单调栈，计算某个高度左右能延伸的最大距离 时间复杂度：O(n) 空间复杂度：O(n)

--- src/main/LargestRectangleInHistogram.java
+++ src/main/LargestRectangleInHistogram.java
@@ -1,6 +1,9 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * 柱状图中最大的矩形：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
@@ -9,32 +12,27 @@ public class LargestRectangleInHistogram {
 
     public static int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int ans = heights[0];
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(heights[0], 1);
-        for (int i = 1; i < n; i++) {
-            int cnt = map.getOrDefault(heights[i], 0) + 1;
-            boolean curUp = heights[i - 1] <= heights[i];
-            for (Iterator<Map.Entry<Integer, Integer>> it = map.entrySet().iterator(); it.hasNext();) {
-                Map.Entry<Integer, Integer> entry = it.next();
-                if (curUp) {
-                    if (entry.getKey() <= heights[i]) {
-                        map.put(entry.getKey(), entry.getValue() + 1);
-                    }
-                } else {
-                    if (entry.getKey() > heights[i]) {
-                        cnt++;
-                    }
-                    it.remove();
-                }
-                if (entry.getKey() * entry.getValue() > ans) {
-                    ans = entry.getKey() * entry.getValue();
-                }
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
             }
-            if (heights[i] * cnt > ans) {
-                ans = heights[i] * cnt;
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
             }
-            map.put(heights[i], cnt);
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
         }
         return ans;
     }
@@ -44,11 +42,12 @@ public class LargestRectangleInHistogram {
         System.out.println(largestRectangleArea(new int[] {2,4}));
         System.out.println(largestRectangleArea(new int[] {1,2,2}));
         System.out.println(largestRectangleArea(new int[] {1,2,3,4,5}));
-        System.out.println(largestRectangleArea(new int[] {0, 9}));
-        System.out.println(largestRectangleArea(new int[] {1, 1}));
-        System.out.println(largestRectangleArea(new int[] {2, 1, 2}));
+        System.out.println(largestRectangleArea(new int[] {0,9}));
+        System.out.println(largestRectangleArea(new int[] {1,1}));
+        System.out.println(largestRectangleArea(new int[] {2,1,2}));
         System.out.println(largestRectangleArea(new int[] {4,2,0,3,2,5}));
         System.out.println(largestRectangleArea(new int[] {2,1,4,5,1,3,3}));
+        System.out.println(largestRectangleArea(new int[] {4,2,0,3,2,4,3,4}));
     }
 
 }
