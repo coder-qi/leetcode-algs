91f915b3e3f00e79935026d321e99ef95763399d
张琪 <zhangqi@smartbi.com.cn>
2022-02-23 18:08:56 +0800

    分隔链表：https://leetcode-cn.com/problems/partition-list/

--- src/main/PartitionList.java
+++ src/main/PartitionList.java
@@ -4,28 +4,21 @@
 public class PartitionList {
 
     public static ListNode partition(ListNode head, int x) {
-        ListNode dummy = new ListNode(Integer.MIN_VALUE, head);
-        ListNode prev = dummy, prevTail = dummy, cur = dummy.next;
-        prev.next = null;
-        while (cur != null) {
-            if (cur.val < x) {
-                ListNode next = prev.next;
-                prev.next = cur;
-                cur = cur.next;
-                prev.next.next = next;
-
-                prev = prev.next;
-                if (prevTail.val < x) {
-                    prevTail = prev;
-                }
+        ListNode smallHead = new ListNode(), small = smallHead;
+        ListNode largeHead = new ListNode(), large = largeHead;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = head;
             } else {
-                prevTail = prevTail.next = cur;
-                cur = cur.next;
-                prevTail.next = null;
+                large.next = head;
+                large = head;
             }
+            head = head.next;
         }
-
-        return dummy.next;
+        small.next = largeHead.next;
+        large.next = null;
+        return smallHead.next;
     }
 
     public static void main(String[] args) {
