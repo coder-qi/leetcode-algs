bcc05fb3e44e40ec9ad379c880e7b6b19a80cd90
coder-qi <zhangfengxian-test>
2022-02-24 23:28:44 +0800

    https://leetcode-cn.com/problems/scramble-string/

---
+++ src/main/ScrambleString.java
@@ -0,0 +1,16 @@
+/**
+ * 扰乱字符串：https://leetcode-cn.com/problems/scramble-string/
+ */
+public class ScrambleString {
+
+    public static boolean isScramble(String s1, String s2) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isScramble("great", "rgeat"));
+        System.out.println(isScramble("abcde", "caebd"));
+        System.out.println(isScramble("a", "a"));
+    }
+
+}
