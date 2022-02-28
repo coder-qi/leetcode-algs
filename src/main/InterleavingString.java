7605088b677715ab4d3c3dd8d7ae45a8069572be
张琪 <zhangqi@smartbi.com.cn>
2022-02-28 18:09:10 +0800

    交错字符串：https://leetcode-cn.com/problems/interleaving-string/

--- src/main/InterleavingString.java
+++ src/main/InterleavingString.java
@@ -10,30 +10,29 @@ public class InterleavingString {
         if (m + n != s3.length()) {
             return false;
         }
-        boolean[][] dp = new boolean[m + 1][n + 1];
+        boolean[] dp = new boolean[n + 1];
 
-        dp[0][0] = true;
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
-        }
+        dp[0] = true;
         for (int j = 1; j <= n; j++) {
-            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
         }
 
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j))
-                    || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            for (int j = 0; j <= n; j++) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j))
+                    || (j > 0 && dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
             }
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 
     public static void main(String[] args) {
         System.out.println(isInterleave("aabcc", "dbbca", "aadbbcbcac")); // true
         System.out.println(isInterleave("aabcc", "dbbca", "aadbbbaccc")); // false
         System.out.println(isInterleave("", "", "")); // true
+        System.out.println(isInterleave("", "", "aaa")); // false
+        System.out.println(isInterleave("a", "", "c")); // false
     }
 
 }
