54fd34fa46e3e93e66818f0efa4089d9a7f4d33c
coder-qi <zhangfengxian-test>
2022-03-03 22:50:53 +0800

    二叉树的最大深度：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
    深度优先搜索，时间复杂度和空间复杂度都是O(n)

---
+++ src/main/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,17 @@
+/**
+ * 二叉树的最大深度：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ */
+public class MaximumDepthOfBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxDepth(TreeNode.of(3,9,20,null,null,15,7)));
+    }
+
+}
