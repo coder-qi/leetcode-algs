77ac00df8d8dd8ee3cd5dbe29790fd646c4ec823
张琪 <zhangqi@smartbi.com.cn>
2022-03-03 11:08:06 +0800

    对称二叉树：https://leetcode-cn.com/problems/symmetric-tree/
    迭代

--- src/main/SymmetricTree.java
+++ src/main/SymmetricTree.java
@@ -1,4 +1,5 @@
-import com.sun.source.tree.Tree;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 对称二叉树：https://leetcode-cn.com/problems/symmetric-tree/
@@ -6,23 +7,37 @@ import com.sun.source.tree.Tree;
 public class SymmetricTree {
 
     public static boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
+        Deque<TreeNode> pStack = new LinkedList<>();
+        Deque<TreeNode> qStack = new LinkedList<>();
+        TreeNode p = root.left;
+        TreeNode q = root.right;
+        while (!pStack.isEmpty() || p != null || !qStack.isEmpty() || q != null) {
+            while (p != null) {
+                pStack.push(p);
+                p = p.left;
+            }
+            while (q != null) {
+                qStack.push(q);
+                q = q.right;
+            }
+            if (pStack.size() != qStack.size()) {
+                return false;
+            }
+            p = pStack.pop();
+            q = qStack.pop();
+            if (p.val != q.val) {
+                return false;
+            }
+            p = p.right;
+            q = q.left;
         }
-        return isSymmetric(root.left, root.right);
-    }
-
-    private static boolean isSymmetric(TreeNode p, TreeNode q) {
-        if (p == null || q == null) {
-            return p == null && q == null;
-        }
-        return p.val == q.val && isSymmetric(p.left, q.right)
-                    && isSymmetric(p.right, q.left);
+        return true;
     }
 
     public static void main(String[] args) {
         System.out.println(isSymmetric(TreeNode.of(1,2,2,3,4,4,3))); // true
         System.out.println(isSymmetric(TreeNode.of(1,2,2,null,3,null,3))); // false
+        System.out.println(isSymmetric(TreeNode.of(2,3,3,4,5,null,4))); // false
     }
 
 }
