77996ff7ea659d5220da66e4406f1813f45a87e4
coder-qi <zhangfengxian-test>
2022-03-06 19:05:25 +0800

    将有序数组转换为二叉搜索树：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/

---
+++ src/main/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,26 @@
+/**
+ * 将有序数组转换为二叉搜索树：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode root = sortedArrayToBST(nums, 0, nums.length - 1);
+        return root;
+    }
+
+    private static TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, left, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortedArrayToBST(new int[] {-10,-3,0,5,9}));
+    }
+
+}
