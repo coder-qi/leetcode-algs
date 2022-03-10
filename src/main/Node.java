485e6923f39588cfad3939ce5dc38b04ac095cd9
张琪 <zhangqi@smartbi.com.cn>
2022-03-10 17:22:33 +0800

    填充每个节点的下一个右侧节点指针：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/

---
+++ src/main/Node.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+
+    public static Node of(Integer... nums) {
+        Node root = new Node(nums[0]);
+        Queue<Node> stack = new LinkedList<>();
+        stack.offer(root);
+        int i = 1;
+        while (i < nums.length && !stack.isEmpty()) {
+            Integer num = nums[i++];
+            Node node = stack.poll();
+            if (num != null) {
+                Node left = new Node(num);
+                node.left = left;
+                stack.offer(left);
+            }
+            if (i < nums.length) {
+                num = nums[i++];
+                if (num != null) {
+                    Node right = new Node(num);
+                    node.right = right;
+                    stack.offer(right);
+                }
+            }
+        }
+        return root;
+    }
+
+}
---
+++ src/main/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,39 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 填充每个节点的下一个右侧节点指针：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+ */
+public class PopulatingNextRightPointersInEachNode {
+
+    public static Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Deque<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            Node prev = null;
+            for (int i = 0; i < count; i++) {
+                Node cur = queue.poll();
+                if (prev != null) {
+                    prev.next = cur;
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                prev = cur;
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(connect(Node.of(1,2,3,4,5,6,7)));
+    }
+
+}
