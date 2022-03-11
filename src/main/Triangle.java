52683ec49dc09417c91cf0ae0c17d9bd6756af9d
coder-qi <zhangfengxian-test>
2022-03-11 22:19:50 +0800

    三角形最小路径和：https://leetcode-cn.com/problems/triangle/

--- src/main/Triangle.java
+++ src/main/Triangle.java
@@ -8,23 +8,23 @@ public class Triangle {
 
     public static int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][n];
-        dp[0][0] = triangle.get(0).get(0);
+        int[] dp = new int[n];
+        dp[0] = triangle.get(0).get(0);
 
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = i; j >= 0; j--) {
                 if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] + triangle.get(i).get(j);
+                    dp[j] = dp[j] + triangle.get(i).get(j);
                 } else if (j == i) {
-                    dp[i][j] = dp[i - 1][j - 1] + triangle.get(i).get(j);
+                    dp[j] = dp[j - 1] + triangle.get(i).get(j);
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+                    dp[j] = Math.min(dp[j - 1], dp[j]) + triangle.get(i).get(j);
                 }
             }
         }
-        int result = dp[n - 1][0];
+        int result = dp[0];
         for (int i = 1; i < n; i++) {
-            result = Math.min(result, dp[n - 1][i]);
+            result = Math.min(result, dp[i]);
         }
         return result;
     }
