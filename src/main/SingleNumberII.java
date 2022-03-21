b0a965848be4f65583edf4dbdaaeb37f75d19bb8
张琪 <zhangqi@smartbi.com.cn>
2022-03-21 16:34:27 +0800

    只出现一次的数字 II：https://leetcode-cn.com/problems/single-number-ii/

--- src/main/SingleNumberII.java
+++ src/main/SingleNumberII.java
@@ -4,20 +4,13 @@
 public class SingleNumberII {
 
     public static int singleNumber(int[] nums) {
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            int cnt = 0;
-            int mask = 1 << i;
-            for (int j = 0; j < nums.length; j++) {
-                if ((mask & nums[j]) != 0) {
-                    cnt++;
-                }
-            }
-            if (cnt % 3 != 0) {
-                res |= mask;
-            }
+        int a = 0, b = 0;
+        for (int num : nums) {
+            int aNext = (~a & b & num) | (a & ~b & ~num), bNext = ~a & (b ^ num);
+            a = aNext;
+            b = bNext;
         }
-        return res;
+        return b;
     }
 
     public static void main(String[] args) {
