7f8bc22a1968b91b5183ed911044f1103ad8c4a4
张琪 <zhangqi@smartbi.com.cn>
2022-03-21 16:41:48 +0800

    只出现一次的数字 II：https://leetcode-cn.com/problems/single-number-ii/

--- src/main/SingleNumberII.java
+++ src/main/SingleNumberII.java
@@ -6,9 +6,8 @@ public class SingleNumberII {
     public static int singleNumber(int[] nums) {
         int a = 0, b = 0;
         for (int num : nums) {
-            int aNext = (~a & b & num) | (a & ~b & ~num), bNext = ~a & (b ^ num);
-            a = aNext;
-            b = bNext;
+            b = ~a & (b ^ num);
+            a = ~b & (a ^ num);
         }
         return b;
     }
