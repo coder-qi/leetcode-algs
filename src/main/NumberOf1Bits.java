c64b170726120f86592acba2afbc754a9fa502d8
coder-qi <zhangfengxian-test>
2022-04-09 12:33:07 +0800

    位1的个数：https://leetcode-cn.com/problems/number-of-1-bits/

--- src/main/NumberOf1Bits.java
+++ src/main/NumberOf1Bits.java
@@ -3,15 +3,19 @@
  */
 public class NumberOf1Bits {
 
+    private static final int M1  = 0x55555555; // 01010101010101010101010101010101
+    private static final int M2  = 0x33333333; // 00110011001100110011001100110011
+    private static final int M4  = 0x0f0f0f0f; // 00001111000011110000111100001111
+    private static final int M8  = 0x00ff00ff; // 00000000111111110000000011111111
+    private static final int M16 = 0x0000ffff; // 00000000000000000000000011111111
+
     public static int hammingWeight(int n) {
-        int result = 0;
-        while (n != 0) {
-            if ((n & 1) == 1) {
-                result++;
-            }
-            n >>>= 1;
-        }
-        return result;
+        n = ((n >>> 1) & M1) + (n & M1);
+        n = ((n >>> 2) & M2) + (n & M2);
+        n = ((n >>> 4) & M4) + (n & M4);
+        n = ((n >>> 8) & M8) + (n & M8);
+        n = ((n >>> 16) & M16) + (n & M16);
+        return n;
     }
 
     public static void main(String[] args) {
