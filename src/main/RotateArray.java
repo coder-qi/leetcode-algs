faea6f226f3078a3176d31436187497d470a8d04
coder-qi <zhangfengxian-test>
2022-04-09 09:47:24 +0800

    轮转数组：https://leetcode-cn.com/problems/rotate-array/

--- src/main/RotateArray.java
+++ src/main/RotateArray.java
@@ -8,22 +8,19 @@ public class RotateArray {
     public static void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        int count = gcd(k, n);
-        for (int start = 0; start < count; start++) {
-            int current = start;
-            int prev = nums[start];
-            do {
-                int next = (current + k) % n;
-                int t = nums[next];
-                nums[next] = prev;
-                prev = t;
-                current = next;
-            } while (start != current);
-        }
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 
-    private static int gcd(int x, int y) {
-        return y > 0 ? gcd(y, x % y) : x;
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     public static void main(String[] args) {
