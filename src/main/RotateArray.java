72c283dbbadd8fd7e0d6830e7b053e418f05a2f0
coder-qi <zhangfengxian-test>
2022-04-09 08:50:14 +0800

    轮转数组：https://leetcode-cn.com/problems/rotate-array/

--- src/main/RotateArray.java
+++ src/main/RotateArray.java
@@ -6,22 +6,18 @@ import java.util.Arrays;
 public class RotateArray {
 
     public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        if (k == 0) {
-            return;
-        }
-        int[] temp = new int[k];
-        // 交换0 ~ k
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[(i + k) % n];
-            nums[(i + k) % n] = nums[i];
-        }
-        // k ~ n
-        for (int i = k; i < n; i++) {
-            int t = nums[(i + k) % n];
-            nums[(i + k) % n] = temp[i % k];
-            temp[i % k] = t;
+        int n = nums.length, left = 0;
+        while (left < n) {
+            int len = n - left;
+            k = k % len;
+            if (k == 0) {
+                break;
+            }
+            for (int i = left, end = left + len - len % k; i < end; i++) {
+                swap(nums, i, left + (i - left) % k + len - k);
+            }
+            left += len - len % k;
+            k = len % k == 0 ? 0 : k % (len % k);
         }
     }
 
@@ -37,6 +33,17 @@ public class RotateArray {
         System.out.println(Arrays.toString(nums)); // [5,6,7,1,2,3,4]
         nums = new int[] {-1,-100,3,99};
         rotate(nums, 2);
+        System.out.println(Arrays.toString(nums));
+        nums = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27};
+        rotate(nums, 38);
+        System.out.println(Arrays.toString(nums));
+
+        nums = new int[] {1,2,3,4,5,6};
+        rotate(nums, 4);
+        System.out.println(Arrays.toString(nums));
+
+        nums = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
+        rotate(nums, 29);
         System.out.println(Arrays.toString(nums));
     }
 
