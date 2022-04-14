a0279c931399e7b9e34e617828ac2ba5351808a4
张琪 <zhangqi@smartbi.com.cn>
2022-04-14 16:30:10 +0800

    快乐数：https://leetcode-cn.com/problems/happy-number/

--- src/main/HappyNumber.java
+++ src/main/HappyNumber.java
@@ -1,26 +1,25 @@
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 快乐数：https://leetcode-cn.com/problems/happy-number/
  */
 public class HappyNumber {
 
     public static boolean isHappy(int n) {
-        Set<Integer> nums = new HashSet<>();
-        while (n != 1) {
-            int v = 0;
-            while (n != 0) {
-                int m = n % 10;
-                v += m * m;
-                n /= 10;
-            }
-            if (!nums.add(v)) {
-                return false;
-            }
-            n = v;
+        int slow = n, fast = nextNumber(n);
+        while (fast != 1 && fast != slow) {
+            slow = nextNumber(slow);
+            fast = nextNumber(nextNumber(fast));
         }
-        return true;
+        return fast == 1;
+    }
+
+    private static int nextNumber(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int m = n % 10;
+            sum += m * m;
+            n /= 10;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
