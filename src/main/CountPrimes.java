1e7728bc5250533304a700dbc89b1bb961dd31c8
张琪 <zhangqi@smartbi.com.cn>
2022-04-15 17:43:27 +0800

    计数质数：https://leetcode-cn.com/problems/count-primes/

--- src/main/CountPrimes.java
+++ src/main/CountPrimes.java
@@ -1,28 +1,28 @@
+import java.util.Arrays;
+
 /**
  * 计数质数：https://leetcode-cn.com/problems/count-primes/
  */
 public class CountPrimes {
 
     public static int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!isPrime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
         int result = 0;
         for (int i = 2; i < n; i++) {
-            if (isPrime(i)) {
+            if (isPrime[i]) {
                 result++;
             }
         }
         return result;
-    }
-
-    private static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2, end = (int) Math.sqrt(n); i <= end; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public static void main(String[] args) {
