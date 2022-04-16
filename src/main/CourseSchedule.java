fc6312ead218b2f3ac93ad645dba9170e4149154
coder-qi <zhangfengxian-test>
2022-04-16 09:56:26 +0800

    课程表：https://leetcode-cn.com/problems/course-schedule/

--- src/main/CourseSchedule.java
+++ src/main/CourseSchedule.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 /**
  * 课程表：https://leetcode-cn.com/problems/course-schedule/
@@ -9,52 +11,44 @@ import java.util.Map;
 public class CourseSchedule {
 
     List<Integer>[] edges;
-    boolean[] marked;
-    boolean[] onStack;
-    boolean hasCycle;
+    int[] indeg;
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if (prerequisites.length <= 1) {
             return true;
         }
         edges = new List[numCourses];
+        indeg = new int[numCourses];
         for (int i = 0; i < prerequisites.length; i++) {
-            addEdge(prerequisites[i][0], prerequisites[i][1]);
+            addEdge(prerequisites[i][1], prerequisites[i][0]);
         }
-
-        marked = new boolean[numCourses];
-        onStack = new boolean[numCourses];
-        for (int v = 0; v < numCourses; v++) {
-            if (!marked[v]) {
-                dfs(v);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
             }
         }
-        return !hasCycle;
-    }
-
-    private void dfs(int v) {
-        if (edges[v] == null) {
-            return;
-        }
-        onStack[v] = true;
-        marked[v] = true;
-        for (int w : edges[v]) {
-            if (hasCycle) {
-                return;
-            }
-            if (!marked[w]) {
-                dfs(w);
-            } else if (onStack[w]) {
-                hasCycle = true;
+        int visited = 0;
+        while (!q.isEmpty()) {
+            visited++;
+            int u = q.poll();
+            if (edges[u] != null) {
+                for (int v : edges[u]) {
+                    indeg[v]--;
+                    if (indeg[v] == 0) {
+                        q.offer(v);
+                    }
+                }
             }
         }
-        onStack[v] = false;
+        return visited == numCourses;
     }
 
     private void addEdge(int x, int y) {
         if (edges[x] == null) {
             edges[x] = new ArrayList<>();
         }
+        indeg[y]++;
         edges[x].add(y);
     }
 
@@ -69,6 +63,11 @@ public class CourseSchedule {
         System.out.println(new CourseSchedule().canFinish(5, new int[][]{
             {1,4},{2,4},{3,1},{3,2}
         })); // true
+        System.out.println(new CourseSchedule().canFinish(3, new int[][]{
+            {1,0},
+            {0,1},
+            {1,2}
+        })); // false
     }
 
 }
