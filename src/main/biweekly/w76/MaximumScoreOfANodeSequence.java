ab05add7bb6b4c672fd5cec859057cfe111c3e88
coder-qi <zhangfengxian-test>
2022-04-17 18:58:55 +0800

    6063. 节点序列的最大得分：https://leetcode-cn.com/problems/maximum-score-of-a-node-sequence/

--- src/main/biweekly/w76/MaximumScoreOfANodeSequence.java
+++ src/main/biweekly/w76/MaximumScoreOfANodeSequence.java
@@ -1,9 +1,9 @@
 package biweekly.w76;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 /**
  * 6063. 节点序列的最大得分：https://leetcode-cn.com/problems/maximum-score-of-a-node-sequence/
@@ -13,20 +13,36 @@ public class MaximumScoreOfANodeSequence {
 
     public static int maximumScore(int[] scores, int[][] edges) {
         int n = scores.length;
-        List<List<Integer>> graph = new ArrayList<>();
+        List<PriorityQueue<int[]>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            graph.add(new PriorityQueue<>(Comparator.comparingInt(a -> a[0])));
         }
-        for (int i = 0; i < edges.length; i++) {
-            graph.get(edges[i][0]).add(edges[i][1]);
-            graph.get(edges[i][1]).add(edges[i][0]);
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            graph.get(a).offer(new int[] {scores[b], b});
+            graph.get(b).offer(new int[] {scores[a], a});
+            if (graph.get(a).size() > 3) {
+                graph.get(a).poll();
+            }
+            if (graph.get(b).size() > 3) {
+                graph.get(b).poll();
+            }
         }
 
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-
+        int ans = -1;
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            for (int[] k : graph.get(a)) {
+                int x = k[1];
+                for (int[] p : graph.get(b)) {
+                    int y = p[1];
+                    if (x != b && y != a && x != y) {
+                        ans = Math.max(ans, scores[x] + scores[a] + scores[b] + scores[y]);
+                    }
+                }
+            }
         }
-        return 0;
+        return ans;
     }
 
     public static void main(String[] args) {
