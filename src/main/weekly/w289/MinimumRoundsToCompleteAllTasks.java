33e412586076783cc848932a41ef61a8ab5be52c
coder-qi <zhangfengxian-test>
2022-04-17 17:14:18 +0800

    力扣第289场周赛

--- src/main/weekly/w289/MinimumRoundsToCompleteAllTasks.java
+++ src/main/weekly/w289/MinimumRoundsToCompleteAllTasks.java
@@ -1,6 +1,7 @@
 package weekly.w289;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 6071. 完成所有任务需要的最少轮数：https://leetcode-cn.com/problems/minimum-rounds-to-complete-all-tasks/comments/
@@ -8,26 +9,22 @@ import java.util.Arrays;
 public class MinimumRoundsToCompleteAllTasks {
 
     public static int minimumRounds(int[] tasks) {
-        Arrays.sort(tasks);
         int n = tasks.length;
-        int left = 0, ans = 0;
-        while (left < n) {
-            int right = left;
-            while (right < n && tasks[left] == tasks[right]) {
-                right++;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(tasks[i], map.getOrDefault(tasks[i], 0) + 1);
+        }
+        int ans = 0;
+        for (int count : map.values()) {
+            if (count == 1) {
+                return -1;
             }
-            int count = right - left;
             if (count % 3 == 1) {
-                if (count == 1) {
-                    return -1;
-                } else {
-                    // 3 * c + 4
-                    ans += (count - 4) / 3 + 2;
-                }
+                // 3 * c + 4
+                ans += (count - 4) / 3 + 2;
             } else {
                 ans += (count + 2) / 3;
             }
-            left = right;
         }
         return ans;
     }
