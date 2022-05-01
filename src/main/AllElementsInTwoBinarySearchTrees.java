fcb31b9534927de91c4cee34f6723f910cc3bf01
coder-qi <zhangfengxian-test>
2022-05-01 19:38:18 +0800

    1305. 两棵二叉搜索树中的所有元素：https://leetcode-cn.com/problems/all-elements-in-two-binary-search-trees/

--- src/main/AllElementsInTwoBinarySearchTrees.java
+++ src/main/AllElementsInTwoBinarySearchTrees.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,20 +7,37 @@ import java.util.List;
 public class AllElementsInTwoBinarySearchTrees {
 
     public static List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> result = new ArrayList<>();
-        dfs(root1, result);
-        dfs(root2, result);
-        Collections.sort(result);
-        return result;
+        List<Integer> nums1 = new ArrayList<>();
+        List<Integer> nums2 = new ArrayList<>();
+        inorder(root1, nums1);
+        inorder(root2, nums2);
+        List<Integer> merged = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (true) {
+            if (p1 == nums1.size()) {
+                merged.addAll(nums2.subList(p2, nums2.size()));
+                break;
+            }
+            if (p2 == nums2.size()) {
+                merged.addAll(nums1.subList(p1, nums1.size()));
+                break;
+            }
+            if (nums1.get(p1) < nums2.get(p2)) {
+                merged.add(nums1.get(p1++));
+            } else {
+                merged.add(nums2.get(p2++));
+            }
+        }
+        return merged;
     }
 
-    private static void dfs(TreeNode root, List<Integer> result) {
+    private static void inorder(TreeNode root, List<Integer> result) {
         if (root == null) {
             return;
         }
-        dfs(root.left, result);
+        inorder(root.left, result);
         result.add(root.val);
-        dfs(root.right, result);
+        inorder(root.right, result);
     }
 
     public static void main(String[] args) {
