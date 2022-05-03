5b11dc8d67651430073a2871d20b9e45f82eade1
coder-qi <zhangfengxian-test>
2022-05-03 11:46:00 +0800

    6050. 字符串的总引力：https://leetcode-cn.com/problems/total-appeal-of-a-string/

--- src/main/weekly/w291/TotalAppealOfAString.java
+++ src/main/weekly/w291/TotalAppealOfAString.java
@@ -1,17 +1,29 @@
 package weekly.w291;
 
+import java.util.Arrays;
+
 /**
  * 6050. 字符串的总引力：https://leetcode-cn.com/problems/total-appeal-of-a-string/
  */
 public class TotalAppealOfAString {
 
     public static long appealSum(String s) {
-        return 0;
+        long ans = 0;
+        int[] pos = new int[26];
+        Arrays.fill(pos, -1);
+        for (int i = 0, sum = 0; i < s.length(); i++) {
+            int j = s.charAt(i) - 'a';
+            sum += i - pos[j];
+            ans += sum;
+            pos[j] = i;
+        }
+        return ans;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(appealSum("abbca"));
+        System.out.println(appealSum("abbca")); // 28
+        System.out.println(appealSum("code")); // 20
     }
 
 }
