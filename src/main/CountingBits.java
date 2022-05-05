54b46623e781f8201b00b48a535076315f70b941
coder-qi <zhangfengxian-test>
2022-05-05 23:36:05 +0800

    338. 比特位计数：https://leetcode-cn.com/problems/counting-bits/

--- src/main/CountingBits.java
+++ src/main/CountingBits.java
@@ -8,7 +8,7 @@ public class CountingBits {
     public static int[] countBits(int n) {
         int[] result = new int[n + 1];
         for (int i = 0; i <= n; i++) {
-            result[i] = Integer.bitCount(i);
+            result[i] = i % 2 == 0 ? result[i / 2] : result[i / 2] + 1;
         }
         return result;
     }
