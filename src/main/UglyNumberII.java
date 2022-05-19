90a3165ca560e4311a0a7d9195236b3b26fecb9a
coder-qi <zhangfengxian-test>
2022-05-19 22:48:23 +0800

    264. 丑数 II：https://leetcode.cn/problems/ugly-number-ii/

--- src/main/UglyNumberII.java
+++ src/main/UglyNumberII.java
@@ -1,36 +1,32 @@
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Set;
-
 /**
  * 264. 丑数 II：https://leetcode.cn/problems/ugly-number-ii/
  */
 public class UglyNumberII {
 
     public static int nthUglyNumber(int n) {
-        int[] factors = {2, 3 ,5};
-        Set<Long> seen = new HashSet<>();
-        PriorityQueue<Long> q = new PriorityQueue<>();
-        seen.add(1L);
-        q.offer(1L);
-        int ugly = 0;
-        for (int i = 0; i < n; i++) {
-            long curr = q.poll();
-            ugly = (int) curr;
-            for (int factor : factors) {
-                long next = factor * curr;
-                if (seen.add(next)) {
-                    q.offer(next);
-                }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(num2, Math.min(num3, num5));
+            if (dp[i] == num2) {
+                p2++;
+            }
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
             }
         }
-        return ugly;
+        return dp[n];
     }
 
     public static void main(String[] args) {
         System.out.println(nthUglyNumber(10)); // 12
         System.out.println(nthUglyNumber(1)); // 1
-        System.out.println(nthUglyNumber(1690)); // 1
+        System.out.println(nthUglyNumber(1690)); // 2123366400
     }
 
 }
