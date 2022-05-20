855f641dbf244778d08614bbd2c05ea5267861b0
coder-qi <zhangfengxian-test>
2022-05-20 23:09:21 +0800

    279. 完全平方数：https://leetcode.cn/problems/perfect-squares/

--- src/main/PerfectSquares.java
+++ src/main/PerfectSquares.java
@@ -4,15 +4,15 @@
 public class PerfectSquares {
 
     public static int numSquares(int n) {
-        int cnt = 0;
-        while (n != 0) {
-            int k = (int) Math.ceil(Math.sqrt(n));
-            while (n - k >= 0) {
-                n -= k;
-                cnt++;
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, f[i - j * j]);
             }
+            f[i] = min + 1;
         }
-        return cnt;
+        return f[n];
     }
 
     public static void main(String[] args) {
