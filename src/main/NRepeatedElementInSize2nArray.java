cb453e8b027cbe1ac716a0f8ffc7ace9f7fd176b
coder-qi <zhangfengxian-test>
2022-05-21 10:32:30 +0800

    961. 在长度 2N 的数组中找出重复 N 次的元素：https://leetcode.cn/problems/n-repeated-element-in-size-2n-array/ 数学

--- src/main/NRepeatedElementInSize2nArray.java
+++ src/main/NRepeatedElementInSize2nArray.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 /**
  * 961. 在长度 2N 的数组中找出重复 N 次的元素：https://leetcode.cn/problems/n-repeated-element-in-size-2n-array/
  */
@@ -7,21 +5,21 @@ public class NRepeatedElementInSize2nArray {
 
     public static int repeatedNTimes(int[] nums) {
         int n = nums.length;
-        Random random = new Random();
-        while (true) {
-            int i = random.nextInt(n);
-            int j = random.nextInt(n);
-            if (i != j && nums[i] == nums[j]) {
-                return nums[i];
+        for (int gap = 1; gap <= 3; gap++) {
+            for (int i = 0; i + gap < n; i++) {
+                if (nums[i] == nums[i + gap]) {
+                    return nums[i + gap];
+                }
             }
         }
+        return -1;
     }
 
     public static void main(String[] args) {
         System.out.println(repeatedNTimes(new int[] {1,2,3,3})); // 3
         System.out.println(repeatedNTimes(new int[] {2,1,2,5,3,2})); // 2
         System.out.println(repeatedNTimes(new int[] {5,1,5,2,5,3,5,4})); // 5
-
+        System.out.println(repeatedNTimes(new int[] {9,5,6,9})); // 9
     }
 
 }
