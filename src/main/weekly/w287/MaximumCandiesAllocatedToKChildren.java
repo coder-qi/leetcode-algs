2559c4f8b1b379559bd0a5ee89ffc02d4f1cf706
coder-qi <zhangfengxian-test>
2022-06-14 21:53:14 +0800

    2226. 每个小孩最多能分到多少糖果：https://leetcode.cn/problems/maximum-candies-allocated-to-k-children/
    二分查找

--- src/main/weekly/w287/MaximumCandiesAllocatedToKChildren.java
+++ src/main/weekly/w287/MaximumCandiesAllocatedToKChildren.java
@@ -1,8 +1,6 @@
 package weekly.w287;
 
-import java.util.Arrays;
-
-import util.ArrayUtils;
+import java.util.stream.IntStream;
 
 import static util.ArrayUtils.array;
 
@@ -12,15 +10,31 @@ import static util.ArrayUtils.array;
 public class MaximumCandiesAllocatedToKChildren {
 
     public static int maximumCandies(int[] candies, long k) {
-        Arrays.sort(candies);
-        int n = candies.length;
-        int a = (int) (k / n) + 1, b = n - (int) (k % n) - 1;
-        return candies[b] / a;
+        int max = IntStream.of(candies).max().getAsInt();
+        int left = 1, right = max + 1;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (check(mid, candies, k)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+
+    private static boolean check(int i, int[] candies, long k) {
+        long result = 0;
+        for (int c : candies) {
+            result += c / i;
+        }
+        return result >= k;
     }
 
     public static void main(String[] args) {
-        System.out.println(maximumCandies(array("[5,8,6]"), 3));
-        System.out.println(maximumCandies(array("[2,5]"), 11));
+        System.out.println(maximumCandies(array("[5,8,6]"), 3)); // 5
+        System.out.println(maximumCandies(array("[2,5]"), 11)); // 0
+        System.out.println(maximumCandies(array("[1,2,6,8,6,7,3,5,2,5]"), 3)); // 6
     }
 
 }
