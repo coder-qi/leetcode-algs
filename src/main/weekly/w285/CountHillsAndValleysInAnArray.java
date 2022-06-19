d588a4afa43cde9f7eb91dfb626a913191db86c3
coder-qi <zhangfengxian-test>
2022-06-19 21:17:53 +0800

    2210. 统计数组中峰和谷的数量：https://leetcode.cn/problems/count-hills-and-valleys-in-an-array/

---
+++ src/main/weekly/w285/CountHillsAndValleysInAnArray.java
@@ -0,0 +1,38 @@
+package weekly.w285;
+
+/**
+ * 2210. 统计数组中峰和谷的数量：https://leetcode.cn/problems/count-hills-and-valleys-in-an-array/
+ */
+public class CountHillsAndValleysInAnArray {
+
+    public static int countHillValley(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            while (j < n && nums[i] == nums[j]) {
+                j++;
+            }
+            if (j < n) {
+                if (nums[i] > nums[i - 1]) {
+                    if (j < n && nums[i] > nums[j]) {
+                        ans++;
+                    }
+                } else {
+                    if (j < n && nums[i] < nums[j]) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
