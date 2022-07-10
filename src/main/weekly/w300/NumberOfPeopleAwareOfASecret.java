951f4da0e835e5bb16cdf6d864220438b28215a6
coder-qi <zhangfengxian-test>
2022-07-10 19:18:38 +0800

    2327. 知道秘密的人数：https://leetcode.cn/problems/number-of-people-aware-of-a-secret/
    双队列模拟

--- src/main/weekly/w300/NumberOfPeopleAwareOfASecret.java
+++ src/main/weekly/w300/NumberOfPeopleAwareOfASecret.java
@@ -1,5 +1,8 @@
 package weekly.w300;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * 2327. 知道秘密的人数：https://leetcode.cn/problems/number-of-people-aware-of-a-secret/
  */
@@ -8,28 +11,46 @@ public class NumberOfPeopleAwareOfASecret {
     static final int MOD = (int) (1e9 + 7);
 
     public static int peopleAwareOfSecret(int n, int delay, int forget) {
-        int[] arr = new int[forget];
-        arr[0] = 1;
+        Queue<Long> delayQ = new ArrayDeque<>(),
+            propagationQ = new ArrayDeque<>();
+        long delayCount = 1, propagationCount = 0;
+        delayQ.offer(1L);
         for (int i = 2; i <= n; i++) {
-            int cnt = 0;
-            for (int j = forget - 2; j >= 0; j--) {
-                arr[j + 1] = arr[j];
-                if (j >= delay - 1) {
-                    cnt = (cnt + arr[j]) % MOD;
+            long count = 0;
+            if (delayQ.size() == delay) {
+                count = delayQ.poll();
+                delayCount -= count;
+                if (delayCount < 0) {
+                    delayCount += MOD;
                 }
             }
-            arr[0] = cnt;
+
+            propagationQ.offer(count);
+            propagationCount = (propagationCount + count) % MOD;
+            if (propagationQ.size() > forget - delay) {
+                propagationCount -= propagationQ.poll();
+                if (propagationCount < 0) {
+                    propagationCount += MOD;
+                }
+            }
+
+            delayCount = (delayCount + propagationCount) % MOD;
+            delayQ.offer(propagationCount);
         }
-        int ans = 0;
-        for (int i = 0; i < arr.length; i++) {
-            ans = (ans + arr[i]) % MOD;
+        long ans = delayCount + propagationCount;
+        if (ans < 0) {
+            ans += MOD;
         }
-        return ans;
+        ans %= MOD;
+        return (int) ans;
     }
 
     public static void main(String[] args) {
         System.out.println(peopleAwareOfSecret(6, 2, 4)); // 5
         System.out.println(peopleAwareOfSecret(4, 1, 3)); // 6
+        System.out.println(peopleAwareOfSecret(1000, 1, 2)); // 2
+        System.out.println(peopleAwareOfSecret(684, 18, 496)); // 653668527
+        System.out.println(peopleAwareOfSecret(289, 7, 23)); // 790409951
     }
 
 }
