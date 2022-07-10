016ab4d404f0fb9dfec19e06898770c144ed3d5c
coder-qi <zhangfengxian-test>
2022-07-10 22:30:42 +0800

    741. 摘樱桃：https://leetcode.cn/problems/cherry-pickup/

---
+++ src/main/CherryPickup.java
@@ -0,0 +1,18 @@
+import static util.ArrayUtils.matrix;
+
+/**
+ * 741. 摘樱桃：https://leetcode.cn/problems/cherry-pickup/
+ */
+public class CherryPickup {
+
+    public int cherryPickup(int[][] grid) {
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(new CherryPickup().cherryPickup(matrix("[[0, 1, -1],[1, 0, -1],[1, 1,  1]]"))); // 5
+    }
+
+}
