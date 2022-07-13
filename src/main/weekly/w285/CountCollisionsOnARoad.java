d9a0c1dd044d84a6390da5dd21db2dc51c410653
张琪 <zhangqi@smartbi.com.cn>
2022-07-13 16:37:01 +0800

    2211. 统计道路上的碰撞次数：https://leetcode.cn/problems/count-collisions-on-a-road/
    栈

--- src/main/weekly/w285/CountCollisionsOnARoad.java
+++ src/main/weekly/w285/CountCollisionsOnARoad.java
@@ -1,14 +1,54 @@
 package weekly.w285;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 2211. 统计道路上的碰撞次数：https://leetcode.cn/problems/count-collisions-on-a-road/
  */
 public class CountCollisionsOnARoad {
 
-    
+    public static int countCollisions(String directions) {
+        int ans = 0;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < directions.length(); i++) {
+            char dir = directions.charAt(i);
+            if (stack.isEmpty()) {
+                if (dir != 'L') {
+                    stack.push(dir);
+                }
+            } else {
+                switch (dir) {
+                    case 'S':
+                        while (!stack.isEmpty() && stack.peek() == 'R') {
+                            stack.pop();
+                            ans++;
+                        }
+                        stack.push(dir);
+                        break;
+                    case 'L':
+                        if (stack.peek() == 'R') {
+                            ans += 2;
+                            stack.pop();
+                            while (!stack.isEmpty() && stack.peek() == 'R') {
+                                stack.pop();
+                                ans++;
+                            }
+                            stack.push('S');
+                        } else {
+                            ans++;
+                        }
+                        break;
+                    default: stack.push(dir);
+                }
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(countCollisions("RLRSLL")); // 5
+        System.out.println(countCollisions("LLRR")); // 0
     }
 
 }
