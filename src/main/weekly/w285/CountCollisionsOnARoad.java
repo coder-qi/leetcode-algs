d4ac42a320f045d4c97af52e3aafd822af01bd4c
coder-qi <zhangfengxian-test>
2022-07-13 22:14:51 +0800

    2211. 统计道路上的碰撞次数：https://leetcode.cn/problems/count-collisions-on-a-road/
    去掉左端的L，右端的R，剩下的相当于统计非S的个数

--- src/main/weekly/w285/CountCollisionsOnARoad.java
+++ src/main/weekly/w285/CountCollisionsOnARoad.java
@@ -1,46 +1,22 @@
 package weekly.w285;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
  * 2211. 统计道路上的碰撞次数：https://leetcode.cn/problems/count-collisions-on-a-road/
  */
 public class CountCollisionsOnARoad {
 
     public static int countCollisions(String directions) {
+        int left = 0, right = directions.length() - 1;
+        while (left < directions.length() && directions.charAt(left) == 'L') {
+            left++;
+        }
+        while (right >= left && directions.charAt(right) == 'R') {
+            right--;
+        }
         int ans = 0;
-        Deque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < directions.length(); i++) {
-            char dir = directions.charAt(i);
-            if (stack.isEmpty()) {
-                if (dir != 'L') {
-                    stack.push(dir);
-                }
-            } else {
-                switch (dir) {
-                    case 'S':
-                        while (!stack.isEmpty() && stack.peek() == 'R') {
-                            stack.pop();
-                            ans++;
-                        }
-                        stack.push(dir);
-                        break;
-                    case 'L':
-                        if (stack.peek() == 'R') {
-                            ans += 2;
-                            stack.pop();
-                            while (!stack.isEmpty() && stack.peek() == 'R') {
-                                stack.pop();
-                                ans++;
-                            }
-                            stack.push('S');
-                        } else {
-                            ans++;
-                        }
-                        break;
-                    default: stack.push(dir);
-                }
+        for (int i = left; i <= right; i++) {
+            if (directions.charAt(i) != 'S') {
+                ans++;
             }
         }
         return ans;
