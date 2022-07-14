e5167578631e56d00bae89aeff70ae303fe6fd00
张琪 <zhangqi@smartbi.com.cn>
2022-07-14 18:31:14 +0800

    kong

---
+++ src/main/weekly/w285/LongestSubstringOfOneRepeatingCharacter.java
@@ -0,0 +1,16 @@
+package weekly.w285;
+
+/**
+ * 2213. 由单个字符重复的最长子字符串：https://leetcode.cn/problems/longest-substring-of-one-repeating-character/
+ */
+public class LongestSubstringOfOneRepeatingCharacter {
+
+    public static int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
