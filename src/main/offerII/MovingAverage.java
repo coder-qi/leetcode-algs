09f1d61a5bfe36cbc59d916a19307d1c3510a118
coder-qi <zhangfengxian-test>
2022-07-16 11:29:08 +0800

    剑指 Offer II 041. 滑动窗口的平均值：https://leetcode.cn/problems/qIsx9U/
    数组

--- src/main/offerII/MovingAverage.java
+++ src/main/offerII/MovingAverage.java
@@ -1,28 +1,29 @@
 package offerII;
 
-import java.util.LinkedList;
-
 /**
  * 剑指 Offer II 041. 滑动窗口的平均值：https://leetcode.cn/problems/qIsx9U/
  */
 public class MovingAverage {
 
     int size;
-    int sum;
-    LinkedList<Integer> vals = new LinkedList<>();
+    int sum, len;
+    int[] vals;
 
     /** Initialize your data structure here. */
     public MovingAverage(int size) {
         this.size = size;
+        vals = new int[size];
     }
 
     public double next(int val) {
-        if (vals.size() == size) {
-            sum -= vals.removeFirst();
+        len++;
+        int index = (len - 1) % size;
+        if (len > size) {
+            sum -= vals[index];
         }
+        vals[index] = val;
         sum += val;
-        vals.add(val);
-        return sum / (double) vals.size();
+        return sum / (double) Math.min(len, size);
     }
 
 }
