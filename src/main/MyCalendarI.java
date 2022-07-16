4715cd95f0eed47eb21f05fe9aefeba3301b61de
coder-qi <zhangfengxian-test>
2022-07-16 23:10:49 +0800

    729. 我的日程安排表 I：https://leetcode.cn/problems/my-calendar-i/
    线段树

--- src/main/MyCalendarI.java
+++ src/main/MyCalendarI.java
@@ -1,36 +1,87 @@
-import java.util.TreeSet;
-
 /**
  * 729. 我的日程安排表 I：https://leetcode.cn/problems/my-calendar-i/
  */
 public class MyCalendarI {
 
-    static class MyCalendar {
-
-        TreeSet<int[]> calendars = new TreeSet<>((a, b) -> {
-            if (a[0] >= b[1]) {
-                return 1;
-            } else if (a[1] <= b[0]) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
-
-        public MyCalendar() {
-        }
-
-        public boolean book(int start, int end) {
-            int[] range = new int[] {start, end};
-            return calendars.add(range);
-        }
-    }
-
     public static void main(String[] args) {
         MyCalendar myCalendar = new MyCalendar();
-        System.out.println(myCalendar.book(10, 20));; // return True
-        System.out.println(myCalendar.book(15, 25));; // return False ，这个日程安排不能添加到日历中，因为时间 15 已经被另一个日程安排预订了。
-        System.out.println(myCalendar.book(20, 30));; // return True ，这个日程安排可以添加到日历中，因为第一个日程安排预订的每个时间都小于 20 ，且不包含时间 20 。
+        System.out.println(myCalendar.book(10, 20)); // return True
+        System.out.println(myCalendar.book(15, 25)); // return False ，这个日程安排不能添加到日历中，因为时间 15 已经被另一个日程安排预订了。
+        System.out.println(myCalendar.book(20, 30)); // return True ，这个日程安排可以添加到日历中，因为第一个日程安排预订的每个时间都小于 20 ，且不包含时间 20 。
+    }
+
+}
+
+class MyCalendar {
+
+    public boolean book(int start, int end) {
+        if (query(root, 0, N, start, end - 1) != 0) {
+            return false;
+        }
+        update(root, 0, N, start, end - 1, 1);
+        return true;
+    }
+
+    static final int N = (int) 1e9;
+
+    static class Node {
+        Node left, right;
+        int val, add;
+    }
+
+    Node root = new Node();
+
+    void update(Node node, int start, int end, int l, int r, int val) {
+        if (l <= start && end <= r) {
+            node.val += val;
+            node.add += val;
+            return;
+        }
+        pushDown(node);
+        int mid = (start + end ) >> 1;
+        if (l <= mid) {
+            update(node.left, start, mid, l, r, val);
+        }
+        if (r > mid) {
+            update(node.right, mid + 1, end, l, r, val);
+        }
+        pullUp(node);
+    }
+
+    int query(Node node, int start, int end, int l, int r) {
+        if (l <= start && end <= r) {
+            return node.val;
+        }
+        pushDown(node);
+        int mid = (start + end ) >> 1, ans = 0;
+        if (l <= mid) {
+            ans = query(node.left, start, mid, l, r);
+        }
+        if (r > mid) {
+            ans = Math.max(ans, query(node.right, mid + 1, end, l, r));
+        }
+        return ans;
+    }
+
+    private void pullUp(Node node) {
+        node.val = Math.max(node.left.val, node.right.val);
+    }
+
+    private void pushDown(Node node) {
+        if (node.left == null) {
+            node.left = new Node();
+        }
+        if (node.right == null) {
+            node.right = new Node();
+        }
+        if (node.add == 0) {
+            return;
+        }
+        node.left.val += node.add;
+        node.right.val += node.add;
+        node.left.add += node.add;
+        node.right.add += node.add;
+        node.add = 0;
     }
 
 }
