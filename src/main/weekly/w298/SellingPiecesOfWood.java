89d52bbeba637bb424dd9f90355d327db5750347
coder-qi <zhangfengxian-test>
2022-07-21 22:32:03 +0800

    2312. 卖木头块：https://leetcode.cn/problems/selling-pieces-of-wood/
    DP枚举优化

--- src/main/weekly/w298/SellingPiecesOfWood.java
+++ src/main/weekly/w298/SellingPiecesOfWood.java
@@ -13,18 +13,16 @@ public class SellingPiecesOfWood {
     }
 
     public static long sellingWood(int m, int n, int[][] prices) {
-        int[][] pr = new int[m + 1][n + 1];
-        for (int[] p : prices) {
-            pr[p[0]][p[1]] = p[2];
-        }
         long[][] f = new long[m + 1][n + 1];
+        for (int[] p : prices) {
+            f[p[0]][p[1]] = p[2];
+        }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                f[i][j] = pr[i][j];
-                for (int k = 1; k < i; k++) {
+                for (int k = 1; k <= i / 2; k++) {
                     f[i][j] = Math.max(f[i][j], f[k][j] + f[i - k][j]);
                 }
-                for (int k = 1; k < j; k++) {
+                for (int k = 1; k <= j / 2; k++) {
                     f[i][j] = Math.max(f[i][j], f[i][k] + f[i][j - k]);
                 }
             }
