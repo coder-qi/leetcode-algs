ad5e20d669701d44646fa70df56a2441e0a34b29
coder-qi <zhangfengxian-test>
2022-09-09 08:54:02 +0800

    1598. 文件夹操作日志搜集器：https://leetcode.cn/problems/crawler-log-folder/

--- src/main/CrawlerLogFolder.java
+++ src/main/CrawlerLogFolder.java
@@ -10,11 +10,11 @@ public class CrawlerLogFolder {
     public static int minOperations(String[] logs) {
         int depth = 0;
         for (String log : logs) {
-            if (log.startsWith("../")) {
+            if (log.equals("../")) {
                 if (depth > 0) {
                     depth--;
                 }
-            } else if (!log.startsWith("./")) {
+            } else if (!log.equals("./")) {
                 depth++;
             }
         }
