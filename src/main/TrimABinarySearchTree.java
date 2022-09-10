711c4c0b025acf20a7d25b7ba0cf92c34c0a7d66
coder-qi <zhangfengxian-test>
2022-09-10 08:55:16 +0800

    669. 修剪二叉搜索树：https://leetcode.cn/problems/trim-a-binary-search-tree/

---
+++ src/main/TrimABinarySearchTree.java
@@ -0,0 +1,27 @@
+import util.TreeNode;
+
+/**
+ * 669. 修剪二叉搜索树：https://leetcode.cn/problems/trim-a-binary-search-tree/
+ */
+public class TrimABinarySearchTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+            return root;
+        }
+    }
+
+}
