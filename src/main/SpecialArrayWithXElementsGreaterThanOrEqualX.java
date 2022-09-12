5970907bdbca2f1614e867e52a70e0ed1507d766
coder-qi <zhangfengxian-test>
2022-09-12 09:56:22 +0800

    1608. 特殊数组的特征值：https://leetcode.cn/problems/special-array-with-x-elements-greater-than-or-equal-x/
    二分

--- src/main/SpecialArrayWithXElementsGreaterThanOrEqualX.java
+++ src/main/SpecialArrayWithXElementsGreaterThanOrEqualX.java
@@ -9,15 +9,23 @@ public class SpecialArrayWithXElementsGreaterThanOrEqualX {
 
     public static int specialArray(int[] nums) {
         int n = nums.length;
-        for (int x = 1; x <= n; x++) {
+        int left = 1, right = n;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+
             int count = 0;
             for (int num : nums) {
-                if (num >= x) {
+                if (num >= mid) {
                     count++;
                 }
             }
-            if (count == x) {
-                return x;
+
+            if (count < mid) {
+                right = mid - 1;
+            } else if (count > mid) {
+                left = mid + 1;
+            } else {
+                return mid;
             }
         }
         return -1;
