9e72a361355a49b4d63f7a9fa91144ca58cde19e
coder-qi <zhangfengxian-test>
2022-09-15 09:23:23 +0800

    319. 灯泡开关：https://leetcode.cn/problems/bulb-switcher/

---
+++ src/main/BulbSwitcher.java
@@ -0,0 +1,13 @@
+/**
+ * 319. 灯泡开关：https://leetcode.cn/problems/bulb-switcher/
+ */
+public class BulbSwitcher {
+
+    public static void main(String[] args) {
+    }
+
+    public static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n + 0.5);
+    }
+
+}
