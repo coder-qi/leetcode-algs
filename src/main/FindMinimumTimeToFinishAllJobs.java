8c4431b1f44ee4063017ad5e579a2aa32a590312
coder-qi <zhangfengxian-test>
2022-09-20 15:06:43 +0800

    1723. 完成所有工作的最短时间：https://leetcode.cn/problems/find-minimum-time-to-finish-all-jobs/
    动态规划 + 状态压缩

--- src/main/FindMinimumTimeToFinishAllJobs.java
+++ src/main/FindMinimumTimeToFinishAllJobs.java
@@ -1,47 +1,33 @@
-import java.util.Arrays;
-
 /**
  * 1723. 完成所有工作的最短时间：https://leetcode.cn/problems/find-minimum-time-to-finish-all-jobs/
  */
 public class FindMinimumTimeToFinishAllJobs {
 
     public static void main(String[] args) {
-
+        System.out.println(minimumTimeRequired(new int[] {3,2,3}, 3));
     }
 
-    public int minimumTimeRequired(int[] jobs, int k) {
-        Arrays.sort(jobs);
-        int left = jobs[jobs.length - 1], right = Arrays.stream(jobs).sum();
-        while (left < right) {
-            int mid = (left + right) >> 1;
-            if (check(jobs, k, mid)) {
-                right = mid;
-            } else {
-                left = mid + 1;
+    public static int minimumTimeRequired(int[] jobs, int k) {
+        int n = jobs.length;
+        int[] sum = new int[1 << n];
+        for (int i = 1; i < (1 << n); i++) {
+            int x = Integer.numberOfTrailingZeros(i), y = i - (1 << x);
+            sum[i] = sum[y] + jobs[x];
+        }
+        int[][] dp = new int[k][1 << n];
+        for (int i = 0; i < (1 << n); i++) {
+            dp[0][i] = sum[i];
+        }
+        for (int i = 1; i < k; i++) {
+            for (int j = 0; j < (1 << n); j++) {
+                int min = Integer.MAX_VALUE;
+                for (int x = j; x != 0; x = (x - 1) & j) {
+                    min = Math.min(min, Math.max(dp[i - 1][j - x], sum[x]));
+                }
+                dp[i][j] = min;
             }
         }
-        return left;
-    }
-
-    private boolean check(int[] jobs, int k, int limit) {
-        return dfs(jobs, new int[k], jobs.length - 1, limit);
-    }
-
-    private  boolean dfs(int[] jobs, int[] workers, int index, int limit) {
-        if (index < 0) {
-            return true;
-        }
-        for (int i = 0; i < workers.length; i++) {
-            workers[i] += jobs[index];
-            if (workers[i] <= limit && dfs(jobs, workers, index - 1, limit)) {
-                return true;
-            }
-            workers[i] -= jobs[index];
-            if (workers[i] == 0 || workers[i] + jobs[index] == limit) {
-                break;
-            }
-        }
-        return false;
+        return dp[k - 1][(1 << n) - 1];
     }
 
 }
