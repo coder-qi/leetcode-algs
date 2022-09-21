edcf649a7c44fc2b60a3eb06cd7eb9d70aac76d7
coder-qi <zhangfengxian-test>
2022-09-21 11:19:31 +0800

    854. 相似度为 K 的字符串：https://leetcode.cn/problems/k-similar-strings/
    dfs

--- src/main/KSimilarStrings.java
+++ src/main/KSimilarStrings.java
@@ -1,8 +1,3 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 854. 相似度为 K 的字符串：https://leetcode.cn/problems/k-similar-strings/
  */
@@ -15,41 +10,55 @@ public class KSimilarStrings {
         System.out.println(new KSimilarStrings().kSimilarity("abac", "baca")); // 2
     }
 
-    public int kSimilarity(String s1, String s2) {
-        Deque<Object[]> q = new ArrayDeque<>();
-        Set<String> visit = new HashSet<>();
-        q.offer(new Object[] {s1, 0});
-        visit.add(s1);
-        int step = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                Object[] pair = q.poll();
-                String cur = (String) pair[0];
-                int pos = (int) pair[1];
-                if (cur.equals(s2)) {
-                    return step;
-                }
-                while (pos < s2.length() && cur.charAt(pos) == s2.charAt(pos)) { // 跳过相同的字符
-                    pos++;
-                }
+    int ans;
 
-                // 找到需要进行交换的字符
-                for (int j = pos + 1; j < s2.length(); j++) {
-                    if (cur.charAt(j) == s2.charAt(j)) {
-                        continue;
-                    }
-                    if (cur.charAt(j) == s2.charAt(pos)) {
-                        String next = swap(cur, pos, j);
-                        if (visit.add(next)) {
-                            q.offer(new Object[] {next, pos + 1});
-                        }
-                    }
-                }
+    public int kSimilarity(String s1, String s2) {
+        StringBuilder sb1 = new StringBuilder(s1.length());
+        StringBuilder sb2 = new StringBuilder(s2.length());
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                sb1.append(s1.charAt(i));
+                sb2.append(s2.charAt(i));
             }
-            step++;
         }
-        return step;
+        ans = Math.max(sb1.length() - 1, 0);
+        dfs(0, 0, sb1.toString(), sb2.toString());
+        return ans;
+    }
+
+    private void dfs(int pos, int cost, String str1, String str2) {
+        if (cost >= ans) {
+            return;
+        }
+        while (pos < str1.length() && str1.charAt(pos) == str2.charAt(pos)) {
+            pos++;
+        }
+        if (pos == str1.length()) {
+            ans = Math.min(ans, cost);
+            return;
+        }
+        if (cost + minSwap(str1, str2, pos) >= ans) {
+            return;
+        }
+        for (int i = pos + 1; i < str1.length(); i++) {
+            if (str1.charAt(i) == str2.charAt(i)) {
+                continue;
+            }
+            if (str1.charAt(i) == str2.charAt(pos)) {
+                String next = swap(str1, i, pos);
+                dfs(pos + 1, cost + 1, next, str2);
+            }
+        }
+    }
+
+    int minSwap(String s1, String s2, int pos) {
+        int total = 0;
+        for (int i = pos; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                total++;
+            }
+        }
+        return (total + 1) / 2;
     }
 
     String swap(String s, int i, int j) {
