1b05e1ec56210087edb5c319b47e07eec070d659
coder-qi <zhangfengxian-test>
2022-09-24 10:04:44 +0800

    1652. 拆炸弹：https://leetcode.cn/problems/defuse-the-bomb/
    和

--- src/main/DefuseTheBomb.java
+++ src/main/DefuseTheBomb.java
@@ -9,6 +9,7 @@ public class DefuseTheBomb {
         System.out.println(Arrays.toString(decrypt(new int[] {5,7,1,4}, 3))); // [12, 10, 16, 13]
         System.out.println(Arrays.toString(decrypt(new int[] {1,2,3,4}, 0))); // [0, 0, 0, 0]
         System.out.println(Arrays.toString(decrypt(new int[] {2,4,9,3}, -2))); // [12,5,6,13]
+        System.out.println(Arrays.toString(decrypt(new int[] {10,5,7,7,3,2,10,3,6,9,1,6}, -4))); // [22,26,22,28,29,22,19,22,18,21,28,19]
     }
 
     public static int[] decrypt(int[] code, int k) {
@@ -17,13 +18,14 @@ public class DefuseTheBomb {
         if (k == 0) {
            return ans;
         }
-        int step = k > 0 ? 1 : -1;
-        for (int i = 0; i < n; i++) {
-            int s = 0;
-            for (int j = 0; j < Math.abs(k); j++) {
-                s += code[(i + step * (j + 1) + n) % n];
-            }
+        int sign = k > 0 ? 1 : -1, s = 0;
+        for (int i = 0, j = k > 0 ? 1 : n + k; i < sign * k - 1; i++) {
+            s += code[(j + i) % n];
+        }
+        for (int i = 0, j = k > 0 ? k : n - 1; i < n; i++, j = (j + 1) % n) {
+            s += code[j % n];
             ans[i] = s;
+            s -= code[(j - (k * sign - 1) + n) % n];
         }
         return ans;
     }
