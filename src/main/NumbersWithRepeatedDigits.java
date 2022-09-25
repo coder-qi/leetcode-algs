8c43a1197011c2f4c4fbc749d330c17d1f21de88
coder-qi <zhangfengxian-test>
2022-09-25 17:57:55 +0800

    1012. 至少有 1 位重复的数字：https://leetcode.cn/problems/numbers-with-repeated-digits/
    数位DP模板题（逆向思维）

--- src/main/NumbersWithRepeatedDigits.java
+++ src/main/NumbersWithRepeatedDigits.java
@@ -17,51 +17,46 @@ public class NumbersWithRepeatedDigits {
     }
 
     List<Integer> digits = new ArrayList<>();
-    int[][][][] memo = new int[10][2][2][1 << 10];
+    int[][][] memo = new int[10][2][1 << 10];
 
     public int numDupDigitsAtMostN(int n) {
-        while (n != 0) {
-            digits.add(n % 10);
-            n /= 10;
+        int t = n;
+        while (t != 0) {
+            digits.add(t % 10);
+            t /= 10;
         }
         Collections.reverse(digits);
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < 2; k++) {
-                    Arrays.fill(memo[i][j][k], -1);
-                }
+                Arrays.fill(memo[i][j], -1);
             }
         }
-        return dfs(0, 1, 1, 0, 0);
+        return n - dfs(0, 1, 1, 0);
     }
 
-    private int dfs(int pos, int bound, int leaderZero, int ok, int mask) {
+    private int dfs(int pos, int bound, int leaderZero, int mask) {
         if (pos == digits.size()) {
-            return leaderZero == 1 ? 0 : ok;
+            return leaderZero == 1 ? 0 : 1;
         }
-        if (memo[pos][bound][ok][mask] != -1) {
-            return memo[pos][bound][ok][mask];
+        if (memo[pos][bound][mask] != -1) {
+            return memo[pos][bound][mask];
         }
         int result = 0;
         if (leaderZero == 1) {
-            result += dfs(pos + 1, 0, leaderZero, ok, mask);
+            result += dfs(pos + 1, 0, leaderZero, mask);
         }
         int up = (bound == 1 ? digits.get(pos) : 9);
         for (int i = leaderZero; i <= up; i++) {
-            boolean matched = (mask & (1 << i)) != 0;
-            if (matched && bound == 0) {
-                result += Math.pow(10, digits.size() - pos - 1);
-            } else {
+            if ((mask & (1 << i)) == 0) {
                 result += dfs(
                     pos + 1,
                     bound == 1 && i == digits.get(pos) ? 1 : 0,
                     0,
-                    ok == 1 || matched ? 1 : 0,
                     mask | (1 << i)
                 );
             }
         }
-        memo[pos][bound][ok][mask] = result;
+        memo[pos][bound][mask] = result;
         return result;
     }
 
