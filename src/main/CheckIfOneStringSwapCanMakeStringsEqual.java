ff7167837f056012bac32ef662a40081f896421c
coder-qi <zhangfengxian-test>
2022-10-11 22:02:04 +0800

    1790. 仅执行一次字符串交换能否使两个字符串相等：https://leetcode.cn/problems/check-if-one-string-swap-can-make-strings-equal/

--- src/main/CheckIfOneStringSwapCanMakeStringsEqual.java
+++ src/main/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -23,10 +23,9 @@ public class CheckIfOneStringSwapCanMakeStringsEqual {
         if (index1 == -1 && index2 == -1) {
             return true;
         }
-        if (index1 == -1 || index2 == -1) {
-            return false;
-        }
-        if (s1.charAt(index1) != s2.charAt(index2) || s1.charAt(index2) != s2.charAt(index1)) {
+        if (index1 == -1 || index2 == -1
+            || s1.charAt(index1) != s2.charAt(index2)
+            || s1.charAt(index2) != s2.charAt(index1)) {
             return false;
         }
         for (int i = index2 + 1; i < n; i++) {
