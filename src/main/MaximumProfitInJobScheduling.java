9f05c836053f278b233469979fed35ef75ae21f8
coder-qi <zhangfengxian-test>
2022-10-23 11:21:05 +0800

    1235. 规划兼职工作：https://leetcode.cn/problems/maximum-profit-in-job-scheduling
    dp + 二分查找

--- src/main/MaximumProfitInJobScheduling.java
+++ src/main/MaximumProfitInJobScheduling.java
@@ -1,7 +1,5 @@
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.stream.IntStream;
 
 /**
  * 1235. 规划兼职工作：https://leetcode.cn/problems/maximum-profit-in-job-scheduling
@@ -9,22 +7,35 @@ import java.util.stream.IntStream;
 public class MaximumProfitInJobScheduling {
 
     public static void main(String[] args) {
-
+        System.out.println(jobScheduling(new int[] {1,2,3,3}, new int[] {3,4,5,6}, new int[] {50,10,40,70}));
     }
 
     public static int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
-        Integer[] idxes = IntStream.range(0, n).boxed().toArray(Integer[]::new);
-        Arrays.sort(idxes, (a, b) -> startTime[a] != startTime[a] ? startTime[a] - startTime[b] : endTime[a] - endTime[b]);
-        int[][] dp = new int[n][2];
-        dp[0][1] = profit[idxes[0]];
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            if (endTime[idxes[i - 1]] <= startTime[idxes[i]]) {
-                dp[i][1] = profit[idxes[i]] + dp[i][0];
+        int[][] jobs = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[] { startTime[i], endTime[i], profit[i] };
+        }
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a[1]));
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int k = binarySearch(jobs, i - 1, jobs[i - 1][0]);
+            dp[i] = Math.max(dp[i - 1], dp[k] + jobs[i - 1][2]);
+        }
+        return dp[n];
+    }
+
+    private static int binarySearch(int[][] jobs, int right, int target) {
+        int left = 0;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (jobs[mid][1] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return Math.max(dp[n - 1][0], dp[n - 1][1]);
+        return left;
     }
 
 }
