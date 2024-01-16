f75873a3d66706212c4df7782fa59dc9dca92fcb
coder-qi <coder-qi@qq.com>
2024-01-16 19:41:47 +0800

    2719. 统计整数数目：https://leetcode.cn/problems/count-of-integers

---
+++ src/main/CountOfIntegers.java
@@ -0,0 +1,16 @@
+/**
+ * 2719. 统计整数数目：https://leetcode.cn/problems/count-of-integers
+ */
+public class CountOfIntegers {
+
+    public static int count(String num1, String num2, int min_sum, int max_sum) {
+        int len = num2.length();
+        int[][] dp = new int[max_sum + 1][len];
+        for (int i = min_sum; i <= max_sum; i++) {
+            for (int j = 0; j < len; j++) {
+
+            }
+        }
+    }
+
+}
