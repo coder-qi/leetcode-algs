c5c2554dbcc9ceee1b1f4ad713cc2ea96a25042b
coder-qi <coder-qi@qq.com>
2024-01-21 12:26:13 +0800

    410. 分割数组的最大值：https://leetcode.cn/problems/split-array-largest-sum/description/
    二分

--- src/main/SplitArrayLargestSum.java
+++ src/main/SplitArrayLargestSum.java
@@ -1,29 +1,37 @@
-import java.util.Arrays;
-
 /**
  * 410. 分割数组的最大值：https://leetcode.cn/problems/split-array-largest-sum/description/
  */
 public class SplitArrayLargestSum {
 
     public int splitArray(int[] nums, int k) {
-        int n = nums.length;
-        int[] preSum = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            preSum[i] = preSum[i - 1] + nums[i - 1];
+        int left = 0, right = 0;
+        for (int num : nums) {
+            right += num;
+            left = Math.max(left, num);
         }
-        int[][] dp = new int[n + 1][k + 1];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        dp[0][0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= Math.min(i, k); j++) {
-                for (int m = 0; m < i; m++) {
-                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[m][j - 1], preSum[i] - preSum[m]));
-                }
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (check(nums, mid, k)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return dp[n][k];
+        return left;
+    }
+
+    private boolean check(int[] nums, int x, int k) {
+        int sum = 0;
+        int count = 1;
+        for (int num : nums) {
+            if (sum + num > x) {
+                sum = num;
+                count++;
+            } else {
+                sum += num;
+            }
+        }
+        return count <= k;
     }
 
     public static void main(String[] args) {
