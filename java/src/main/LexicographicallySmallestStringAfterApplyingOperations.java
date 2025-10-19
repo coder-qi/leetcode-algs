a04337bea4817c40a67658f639f2ec501580865a
zhangqi <coder-qi@qq.com>
2025-10-19 16:13:33 +0800

    1625. 执行操作后字典序最小的字符串：/leetcode.cn/problems/lexicographically-smallest-string-after-applying-operations
    暴力

---
+++ java/src/main/LexicographicallySmallestStringAfterApplyingOperations.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+/**
+ * 1625. 执行操作后字典序最小的字符串：/leetcode.cn/problems/lexicographically-smallest-string-after-applying-operations
+ */
+public class LexicographicallySmallestStringAfterApplyingOperations {
+
+    public String findLexSmallestString(String s, int a, int b) {
+       HashSet<String> all = new HashSet<>(64);
+       all.add(s);
+       HashSet<String> nextSet = new HashSet<>(64);
+       nextSet.add(s);
+       String ans = s;
+       while (!nextSet.isEmpty()) {
+           List<String> list = new ArrayList<>(nextSet);
+           nextSet.clear();
+           for (String str : list) {
+               if (str.compareTo(ans) < 0) {
+                   ans = str;
+               }
+               String s1 = add(str, a);
+               if (all.add(s1)) {
+                   nextSet.add(s1);
+               }
+               String s2 = rotate(str, b);
+               if (all.add(s2)) {
+                   nextSet.add(s2);
+               }
+           }
+       }
+       return ans;
+    }
+
+    private String rotate(String s, int b) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(s.length());
+        for (int i = 0; i < b; i++) {
+            sb.append(s.charAt(n - b + i));
+        }
+        for (int i = 0; i < n - b; i++) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    private String add(String s, int a) {
+        StringBuilder sb = new StringBuilder(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            if ((i & 1) == 0) {
+                sb.append(s.charAt(i));
+            } else {
+                int f = s.charAt(i) - '0' + a;
+                sb.append(f >= 10 ? f - 10 : f);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        LexicographicallySmallestStringAfterApplyingOperations app = new LexicographicallySmallestStringAfterApplyingOperations();
+        System.out.println(app.findLexSmallestString("5525", 9, 2)); // 2050
+        System.out.println(app.findLexSmallestString("74", 5, 1)); // 24
+        System.out.println(app.findLexSmallestString("0011", 4, 2)); // 0011
+        System.out.println(app.findLexSmallestString("43987654", 7, 3)); // 00553311
+        System.out.println(app.findLexSmallestString("593290172167", 7, 4)); // 206658319916
+        System.out.println(app.findLexSmallestString("0828265976149473834240719308951729926225100715191213267697293340972124", 1, 19)); // 0000831589676556524225375807799017444079289979194950858790115227903068
+    }
+
+}
