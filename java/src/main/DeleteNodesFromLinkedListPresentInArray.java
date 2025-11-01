e4289e22f68251377296356471f990cfb26840b5
zhangqi <coder-qi@qq.com>
2025-11-01 21:20:26 +0800

    3217. 从链表中移除在数组中存在的节点：https://leetcode.cn/problems/delete-nodes-from-linked-list-present-in-array
    哈希

---
+++ java/src/main/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,26 @@
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 3217. 从链表中移除在数组中存在的节点：https://leetcode.cn/problems/delete-nodes-from-linked-list-present-in-array
+ */
+public class DeleteNodesFromLinkedListPresentInArray {
+
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> numSet = IntStream.of(nums)
+                .boxed()
+                .collect(Collectors.toSet());
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        while (prev.next != null) {
+            if (numSet.contains(prev.next.val)) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dummy.next;
+    }
+
+}
