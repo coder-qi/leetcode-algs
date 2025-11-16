ae23a1540ae172837bc9f477d2253659e2cd7925
zhangqi <coder-qi@qq.com>
2025-11-16 11:01:22 +0800

    2062. 统计字符串中的元音子字符串：https://leetcode.cn/problems/count-vowel-substrings-of-a-string
    暴力

--- java/src/main/CountVowelSubstringsOfAString.java
+++ java/src/main/CountVowelSubstringsOfAString.java
@@ -1,4 +1,4 @@
-import java.util.Set;
+import java.util.Map;
 
 /**
  * 2062. 统计字符串中的元音子字符串：https://leetcode.cn/problems/count-vowel-substrings-of-a-string
@@ -7,37 +7,28 @@ public class CountVowelSubstringsOfAString {
 
     public int countVowelSubstrings(String word) {
         char[] chars = word.toCharArray();
-        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        Map<Character, Integer> vowels = Map.of(
+                'a', 1,
+                'e', 2,
+                'i', 4,
+                'o', 8,
+                'u', 16);
+        int allMask = (1 << 5) - 1;
         int ans = 0;
         for (int i = 0; i < chars.length; i++) {
-            int[] counts = new int[26];
+            int mask = 0;
             for (int j = i; j < chars.length; j++) {
-                char ch = chars[j];
-                counts[ch - 'a']++;
-                if (hasOnlyVowels(vowels, counts)) {
-                    ans++;
+                if (vowels.containsKey(chars[j])) {
+                    mask |= vowels.get(chars[j]);
+                    if (mask == allMask) {
+                        ans++;
+                    }
+                } else {
+                    break;
                 }
             }
         }
         return ans;
-    }
-
-    private boolean hasOnlyVowels(Set<Character> vowels, int[] counts) {
-        for (char vowel : vowels) {
-            if (counts[vowel - 'a'] == 0) {
-                return false;
-            }
-        }
-        for (int i = 0; i < 26; i++) {
-            char ch = (char) ('a' + i);
-            if (vowels.contains(ch)) {
-                continue;
-            }
-            if (counts[ch - 'a'] > 0) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public static void main(String[] args) {
