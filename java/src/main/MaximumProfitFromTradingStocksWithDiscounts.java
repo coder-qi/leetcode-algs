751f12d51c352308b186182eea53a399cd80f900
zhangqi <zhangqi@winhong.com>
2025-12-16 22:21:11 +0800

    暂存

--- java/src/main/MaximumProfitFromTradingStocksWithDiscounts.java
+++ java/src/main/MaximumProfitFromTradingStocksWithDiscounts.java
@@ -1,3 +1,5 @@
+import util.ArrayUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,28 +16,35 @@ public class MaximumProfitFromTradingStocksWithDiscounts {
         for (int[] h : hierarchy) {
             g[h[0] - 1].add(h[1] - 1);
         }
-        return dfs(g, 0, false, budget, present, future);
+        return dfs(g, 0, 0, budget, present, future);
     }
 
-    private int dfs(List<Integer>[] g, int u, boolean b, int budget, int[] present, int[] future) {
+    private int dfs(List<Integer>[] g, int u, int bossBuy, int budget, int[] present, int[] future) {
         if (budget == 0) {
             return 0;
         }
         int res1 = 0;
+        int p = bossBuy == 1 ? present[u] / 2 : present[u];
         // 购买
-        if (present[u] <= budget) {
-            int f = b ? future[u] / 2 : future[u];
-            res1 = f - present[u];
+        if (p <= budget) {
+            res1 = future[u] - p;
             for (int v : g[u]) {
-                res1 += dfs(g, v, true, budget - f, present, future);
+                res1 += dfs(g, v, 1, budget - p, present, future);
             }
         }
         // 不买
         int res2 = 0;
         for (int v : g[u]) {
-            res2 += dfs(g, v, false, budget, present, future);
+            res2 += dfs(g, v, 0, budget, present, future);
         }
         return Math.max(res1, res2);
+    }
+
+    public static void main(String[] args) {
+        MaximumProfitFromTradingStocksWithDiscounts app = new MaximumProfitFromTradingStocksWithDiscounts();
+        System.out.println(app.maxProfit(3, ArrayUtils.array("[4,6,8]"),
+                ArrayUtils.array("[7,9,11]"),
+                ArrayUtils.matrix("[[1,2],[1,3]]"), 10));
     }
 
 }
