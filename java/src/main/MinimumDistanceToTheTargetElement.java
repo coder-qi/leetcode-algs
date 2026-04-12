e0ddf39e0daf816d3506221081222478929929c0
zhangqi <coder-qi@qq.com>
2026-04-13 00:32:34 +0800

    1848. 到目标元素的最小距离：https://leetcode.cn/problems/minimum-distance-to-the-target-element

---
+++ java/src/main/MinimumDistanceToTheTargetElement.java
@@ -0,0 +1,16 @@
+/**
+ * 1848. 到目标元素的最小距离：https://leetcode.cn/problems/minimum-distance-to-the-target-element
+ */
+public class MinimumDistanceToTheTargetElement {
+
+    public int getMinDistance(int[] nums, int target, int start) {
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                ans = Math.min(ans, Math.abs(i - start));
+            }
+        }
+        return ans;
+    }
+
+}
