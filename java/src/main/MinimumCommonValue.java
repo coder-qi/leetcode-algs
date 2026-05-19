151587721a296372657b671ce7a368489396747d
theLovelyCode <12317464+thelovelycode@user.noreply.gitee.com>
2026-05-19 11:33:44 +0800

    2540. 最小公共值：https://leetcode.cn/problems/minimum-common-value

---
+++ java/src/main/MinimumCommonValue.java
@@ -0,0 +1,20 @@
+/**
+ * 2540. 最小公共值：https://leetcode.cn/problems/minimum-common-value
+ */
+public class MinimumCommonValue {
+
+    public int getCommon(int[] nums1, int[] nums2) {
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length;) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+
+}
